1b568fc62c3dc5066f87acf27be9e67e0b1f26c6
ang67 <binikouadiojosias@gmail.com>
2019-03-20 12:53:33 +0000

    Configure app name

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
    <div class="container">
-       <h2>What is Laracarte?</h2>
-       <p>Laracarte is a clone app of <a href="https://laramap.com" target="_blank"> Laramap.com</a>.</p>
+       <h2>What is {{ config('app.name') }}?</h2>
+       <p>{{ config('app.name') }} is a clone app of <a href="https://laramap.com" target="_blank"> Laramap.com</a>.</p>
 
        <div class="row">
            <div class="col-md-6">
@@ -28,7 +28,7 @@
 
         <hr>
 
-        <h2>Which tools and services are used in Laracarte?</h2>
+        <h2>Which tools and services are used in {{ config('app.name') }}?</h2>
         <p>Basically it's built on Laravel &amp; Bootstrap. But there's a bunch of services used 
             for email and other sections.</p>
 
